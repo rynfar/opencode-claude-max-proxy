89484e5f01f6b18a3d685eb0658de3e432dbf489
Aleksandr Beshkenadze <beshkenadze@gmail.com>
2026-04-05 15:50:11 +0300

    refactor(telemetry): update barrel exports, fix route imports to use index

--- src/telemetry/index.ts
+++ src/telemetry/index.ts
@@ -1,6 +1,13 @@
-export { telemetryStore, TelemetryStore } from "./store"
-export { diagnosticLog, DiagnosticLogStore } from "./logStore"
+export { telemetryStore, MemoryTelemetryStore, TelemetryStore } from "./store"
+export { diagnosticLog, MemoryDiagnosticLogStore, DiagnosticLogStore } from "./logStore"
 export { createTelemetryRoutes } from "./routes"
 export { landingHtml } from "./landing"
-export type { RequestMetric, TelemetrySummary, PhaseTiming } from "./types"
-export type { DiagnosticLog } from "./logStore"
+export { computePercentiles, computeSummary } from "./percentiles"
+export type {
+  RequestMetric,
+  TelemetrySummary,
+  PhaseTiming,
+  ITelemetryStore,
+  IDiagnosticLogStore,
+  DiagnosticLog,
+} from "./types"
